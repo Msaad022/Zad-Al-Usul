8bae265bdd25e92b759a713a37bdafa02c81e95a
Msaad022 <mohamedsad7090@gmail.com>
2026-02-16 18:50:21 +0200

    feat: add initial seed data to astro:db

--- db/config.ts
+++ db/config.ts
@@ -1,50 +1,21 @@
-import { defineDb, defineTable, column } from "astro:db";
+import { defineDb, defineTable, column, NOW } from "astro:db";
 
-// https://astro.build/db/config
-
-const Question = defineTable({
+const Questions = defineTable({
   columns: {
-    id: column.text({ primaryKey: true }),
+    id: column.number({ primaryKey: true }),
     type_que: column.text(),
     question: column.text(),
     answer: column.text(),
     audio_url: column.text(),
     secure_url: column.text(),
     words: column.json(),
-  },
-});
-
-const MainEntity = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true, autoIncrement: true }),
-    question_id: column.text({ references: () => Question.columns.id }),
-    comment: column.text(),
-    acceptedAnswer: column.text(),
-  },
-});
-
-const Description = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true, autoIncrement: true }),
-    mainEntity_id: column.number({ references: () => MainEntity.columns.id }),
-    type: column.text({ enum: ["desQuestion", "desAnswer"] }),
-    alternateName: column.text(),
-  },
-});
-
-const ListItem = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true, autoIncrement: true }),
-    description_id: column.number({ references: () => Description.columns.id }),
-    item: column.text(),
+    description: column.text(),
+    published: column.date({ default: NOW }),
   },
 });
 
 export default defineDb({
   tables: {
-    Question,
-    MainEntity,
-    Description,
-    ListItem,
+    Questions,
   },
 });
